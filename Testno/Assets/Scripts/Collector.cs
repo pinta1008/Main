bc08436fb9fbac584cb721f43f7f63ac29be1d0d
pinta1008 <57998225+pinta1008@users.noreply.github.com>
2019-12-13 12:39:15 +0100

    Jes

---
+++ Testno/Assets/Scripts/Collector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collector : MonoBehaviour
+{
+    public int skupljeni_Coinsi;
+    public Text textCoins;
+    public AudioSource zvuk;
+    int brojCoinsaNaSceni;
+    public Text textTime;
+    public float vrijeme;
+
+    private void Start()
+    {
+        brojCoinsaNaSceni = GameObject.FindGameObjectsWithTag("Collectable").Length;
+        textCoins.text = skupljeni_Coinsi + "/" + brojCoinsaNaSceni;
+        vrijeme = brojCoinsaNaSceni * 45;
+        
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Collectable")
+        {
+            Destroy(other.gameObject);
+            skupljeni_Coinsi++;
+            textCoins.text = skupljeni_Coinsi + "/" + brojCoinsaNaSceni;
+            zvuk.Play();
+        }
+    }
+
+    void Update()
+    {
+        vrijeme -= 1f * Time.deltaTime;
+        textTime.text = vrijeme.ToString("0") + " ";
+       
+    }
+
+
+
+}
---
+++ Testno/Assets/Scripts/Rotacion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+    public float rotateSpeed = 100f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
+    }
+}
